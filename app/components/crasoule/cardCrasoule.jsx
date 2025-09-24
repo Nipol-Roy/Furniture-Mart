637b07b1976877b0ea1a95402a1a83d7b17e9253
Nipol-Roy <nipolroy5@gmail.com>
2025-09-25 00:58:39 +0600

    modify file

--- app/components/crasoule/cardCrasoule.jsx
+++ app/components/crasoule/cardCrasoule.jsx
@@ -30,32 +30,20 @@ const CardCrasoule = ({ title, cards }) => {
   let settings = {
     infinite: false,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
+        breakpoint: 1024, // <= 1024px
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 640, // <= 640px
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
         },
       },
     ],
